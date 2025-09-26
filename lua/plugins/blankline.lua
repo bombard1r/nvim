3213f2012b709f1b3aeb20a3e70adc559176f9e5
jou <losev2300@gmail.com>
2025-09-26 11:48:06 +0200

    better blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -3,11 +3,13 @@ return {
     main = "ibl",
     opts = {
         indent = {
-            char = " ", -- Empty/invisible character for regular indents
+            char = "▏", -- Empty/invisible character for regular indents
+            highlight = "IndentGray", -- Highlight group for regular indents
         },
         scope = {
             enabled = true,
             char = "│", -- Character for current scope
+            highlight = "IblScope", -- Highlight group for current scope
             show_start = true, -- Show start of scope
             show_end = false, -- You can set this to true if you want
             include = {
@@ -19,4 +21,14 @@ return {
             },
         },
     },
+
+    config = function(_, opts)
+        local hooks = require "ibl.hooks"
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "IndentGray", { fg = "#3c3836" }) -- Gray for regular lines
+            vim.api.nvim_set_hl(0, "IblScope", { fg = "#458588" }) -- Blue for current scope
+        end)
+
+        require("ibl").setup(opts)
+    end,
 }
